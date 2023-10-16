e4ff556df83c29265f176aef7bca7582d8482e65
Yuri Pires <yuripiresbr@gmail.com>
2023-10-16 19:46:02 -0300

    style: changed theme names

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -3,5 +3,9 @@ import { useRouter } from "next/router";
 export default function Product() {
   const { query } = useRouter();
 
-  return <h1 className="font-roboto">Product: {JSON.stringify(query)}</h1>;
+  return (
+    <div>
+      <h1>Product</h1>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,12 +12,14 @@ const config: Config = {
         roboto: ["Roboto", "sans-serif"],
       },
       colors: {
-        principal: "#00875F",
-        secondary: "#00B37E",
-        background: "#121214",
-        elements: "#202024",
-        text: "#C4C4CC",
-        title: "#E1E1E6",
+        green500: "#00875F",
+        green300: "#00B37E",
+
+        gray900: "#121214",
+        gray800: "#202024",
+        gray300: "#C4C4CC",
+        gray100: "#E1E1E6",
+
         white: "#FFFFFF",
       },
     },
