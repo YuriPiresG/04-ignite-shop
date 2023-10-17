b7896fa4bd536cc5d867a8f093acccc8148db408
Yuri Pires <yuripiresbr@gmail.com>
2023-10-16 21:32:29 -0300

    feat: added new home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Image from "next/image";
+
+import logoImg from "../assets/logo.svg";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className=" flex min-h-screen flex-col  items-start justify-center">
+      <header className="mx-auto w-full max-w-6xl py-8">
+        <Image src={logoImg} alt="Logo Ignite Shop" />
+      </header>
+
+      <Component {...pageProps} />
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,35 @@
 import { Inter } from "next/font/google";
+import Image from "next/image";
+
+import camisa1 from "../assets/camisa1.png";
+import camisa2 from "../assets/camisa2.png";
+import camisa3 from "../assets/camisa3.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return (
+    <main className="max-w-mainMaxWidth min-h-mainMinHeight ml-auto flex w-full gap-12">
+      <a
+        href=""
+        className="from-custom-start to-custom-end group relative flex cursor-pointer items-center justify-center rounded-lg bg-gradient-to-b object-cover p-1"
+      >
+        <Image src={camisa1} alt="Camisa 1" width={520} height={480} />
+        <footer className="bg-footerRgba translate-y-hoverFooter absolute bottom-1 left-1 right-1 flex items-center justify-between rounded-md p-8 opacity-0 transition-all ease-in-out group-hover:translate-y-0 group-hover:opacity-100">
+          <strong className="text-lg">Camiseta X</strong>
+          <span className="text-xl font-bold text-green300">R$ 79,90</span>
+        </footer>
+      </a>
+      <a
+        href=""
+        className="from-custom-start to-custom-end group relative flex cursor-pointer items-center justify-center rounded-lg bg-gradient-to-b object-cover p-1"
+      >
+        <Image src={camisa2} alt="Camisa 2" width={520} height={480} />
+        <footer className="bg-footerRgba translate-y-hoverFooter absolute bottom-1 left-1 right-1 flex items-center justify-between rounded-md p-8 opacity-0 transition-all ease-in-out group-hover:translate-y-0 group-hover:opacity-100">
+          <strong className="text-lg">Camiseta X</strong>
+          <span className="text-xl font-bold text-green300">R$ 79,90</span>
+        </footer>
+      </a>
+    </main>
+  );
 }
