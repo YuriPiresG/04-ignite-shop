90dfa5339e9522e7c1cf6297027301304a662e55
Yuri Pires <yuripiresbr@gmail.com>
2023-10-18 10:57:01 -0300

    feat: added logic to the product page and it now redirect to stripe

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -5,7 +5,15 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const priceId = "";
+  const { priceId } = req.body;
+
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed." });
+  }
+
+  if (!priceId) {
+    return res.status(400).json({ error: "Price not found." });
+  }
 
   const successUrl = `${process.env.NEXT_URL}/success`;
   const cancelUrl = `${process.env.NEXT_URL}/`;
@@ -20,6 +28,9 @@ export default async function handler(
         quantity: 1,
       },
     ],
-    payment_method_types: ["card"],
+  });
+
+  return res.status(201).json({
+    checkoutUrl: checkoutSession.url,
   });
 }
